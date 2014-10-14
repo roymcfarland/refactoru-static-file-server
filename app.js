bbc1e1cabcf7c4c6c9fe9f39e6475af10308678e
Roy McFarland <roy.mcfarland@gmail.com>
2014-10-14 13:11:20 -0600

    edited

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
 app.set('view engine', 'jade');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res) {
-	res.render('index');
+	fs.readFile('./data.txt', function(err, data){
+		res.header('Content-Type', 'text/html');
+		res.send(data);
+	});
 });
 
 var server = app.listen(4914, function() {
